d7b9d30240cd0361adb9ff9eeb35b669acc1dc70
yousef abea allah atyyeh samara <yousef.aa.samara@gmail.com>
2021-02-15 19:54:00 +0200

    v1

---
+++ js/code.js
@@ -0,0 +1,62 @@
+'use strict';
+let yourn ='';
+yourn = prompt('inter your name');
+alert('welcome '+ yourn);
+let myName='';
+while(myName !== 'jilldshisdhud')
+{
+  myName = prompt('is my name yousef?');
+  myName = myName.toLowerCase();
+  if(myName === 'yes' || myName==='no' || myName==='y' || myName==='n' )
+  {
+    break;
+  }
+}
+if(myName === 'yes' || myName==='y' )
+{console.log('right');
+  alert('right');}
+else{console.log('wrong');
+  alert('wrong');} //yes
+while(myName !== 'jilldshisdhud')
+{
+  myName = prompt('am i fat?');
+  myName = myName.toLowerCase();
+  if(myName === 'yes' || myName==='no' || myName==='y' || myName==='n' )
+  {
+    break;
+  }
+}
+if(myName === 'yes' || myName==='y' )
+{console.log('right');
+  alert('right');}
+else{console.log('wrong');
+  alert('wrong');}
+while(myName !== 'jilldshisdhud')
+{
+  myName = prompt('am i tall?');
+  myName = myName.toLowerCase();
+  if(myName === 'yes' || myName==='no' || myName==='y' || myName==='n' )
+  {
+    break;
+  }
+}
+if(myName === 'yes' || myName==='y' )
+{console.log('right');
+  alert('right');}
+else{console.log('wrong');
+  alert('wrong');}
+while(myName !== 'jilldshisdhud')
+{
+  myName = prompt('am i superman?');
+  myName = myName.toLowerCase();
+  if(myName === 'yes' || myName==='no' || myName==='y' || myName==='n' )
+  {
+    break;
+  }
+}
+if(myName === 'yes' || myName==='y' )
+{console.log('right');
+  alert('right');}
+else{console.log('wrong');
+  alert('wrong');}
+alert('come bace agane '+ yourn);
